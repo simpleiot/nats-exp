d96c7b1cbe97a3a83d7802f692832603c58783ae
Cliff Brake <cbrake@bec-systems.com>
2023-12-14 11:31:37 -0500

    source messages from leaf to hub node

--- bi-directional-sync/main.go
+++ bi-directional-sync/main.go
@@ -61,6 +61,17 @@ func main() {
 		return
 	}
 
+	err = leafCreateStream(srv)
+	if err != nil {
+		fmt.Printf("Error creating leaf stream: %v\n", err)
+		return
+	}
+
+	err = hubSourceLeafStream(srvLeaf)
+	if err != nil {
+		fmt.Printf("Error hub sourcing leaf stream: %v\n", err)
+		return
+	}
 }
 
 func hubInit() (*server.Server, string, error) {
@@ -90,6 +101,7 @@ func leafInit() (*server.Server, string, error) {
 	ol := test.DefaultTestOptions
 	ol.Port = -1
 	ol.JetStream = true
+	ol.JetStreamDomain = "leaf"
 	ol.LeafNode.Remotes = []*server.RemoteLeafOpts{
 		{
 			URLs: []*url.URL{u},
@@ -142,8 +154,8 @@ func hubCreateStream(srv *server.Server) error {
 	}
 
 	cfg := jetstream.StreamConfig{
-		Name:     "NODES",
-		Subjects: []string{"n.>"},
+		Name:     "NODES-HUB",
+		Subjects: []string{"n.hub.>"},
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -154,10 +166,10 @@ func hubCreateStream(srv *server.Server) error {
 		return fmt.Errorf("Error creating stream on hub: %w", err)
 	}
 
-	_, _ = js.Publish(ctx, "n.123.value", []byte("12"))
-	_, _ = js.Publish(ctx, "n.123.value", []byte("13"))
-	_, _ = js.Publish(ctx, "n.123.value", []byte("14"))
-	_, _ = js.Publish(ctx, "n.123.value", []byte("15"))
+	_, _ = js.Publish(ctx, "n.hub.123.value", []byte("12"))
+	_, _ = js.Publish(ctx, "n.hub.123.value", []byte("13"))
+	_, _ = js.Publish(ctx, "n.hub.123.value", []byte("14"))
+	_, _ = js.Publish(ctx, "n.hub.123.value", []byte("15"))
 
 	si, err := stream.Info(ctx, jetstream.WithSubjectFilter("n.123.>"))
 	if err != nil {
@@ -189,13 +201,13 @@ func leafConnectHubStream(srv *server.Server) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	stream, err := js.Stream(ctx, "NODES")
+	stream, err := js.Stream(ctx, "NODES-HUB")
 
 	if err != nil {
 		return fmt.Errorf("Error opening stream: %w", err)
 	}
 
-	si, err := stream.Info(ctx, jetstream.WithSubjectFilter("n.123.>"))
+	si, err := stream.Info(ctx, jetstream.WithSubjectFilter("n.hub.123.>"))
 	if err != nil {
 		return fmt.Errorf("Error getting stream info: %w", err)
 	}
@@ -217,17 +229,17 @@ func leafSourceHubStream(srv *server.Server) error {
 		_ = nc.Drain()
 	}()
 
-	js, err := jetstream.New(nc)
+	js, err := jetstream.NewWithDomain(nc, "leaf")
 	if err != nil {
 		return fmt.Errorf("Error creating Jetstream: %w", err)
 	}
 
 	cfg := jetstream.StreamConfig{
-		Name:     "NODES",
-		Subjects: []string{"n.>"},
+		Name:     "NODES-HUB",
+		Subjects: []string{"n.hub.>"},
 		Sources: []*jetstream.StreamSource{
 			{
-				Name:   "NODES",
+				Name:   "NODES-HUB",
 				Domain: "hub",
 			},
 		},
@@ -251,7 +263,108 @@ func leafSourceHubStream(srv *server.Server) error {
 			return fmt.Errorf("Returned wrong number of messages: %v", si.State.Msgs)
 		}
 
-		log.Println("Number of leaf sourced stream messages: ", si.State.Msgs)
+		log.Println("Number of hub->leaf sourced stream messages: ", si.State.Msgs)
+		return nil
+	})
+
+	return nil
+}
+
+func leafCreateStream(srv *server.Server) error {
+	url := srv.ClientURL()
+
+	nc, err := nats.Connect(url)
+	if err != nil {
+		return fmt.Errorf("Error connecting: %w", err)
+	}
+
+	defer func() {
+		_ = nc.Drain()
+	}()
+
+	js, err := jetstream.NewWithDomain(nc, "leaf")
+	if err != nil {
+		return fmt.Errorf("Error creating Jetstream: %w", err)
+	}
+
+	// The stream name and subjects must not conflict with what already exists
+
+	cfg := jetstream.StreamConfig{
+		Name:     "NODES-LEAF",
+		Subjects: []string{"n.leaf.>"},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := js.CreateStream(ctx, cfg)
+	if err != nil {
+		return fmt.Errorf("Error creating stream on leaf: %w", err)
+	}
+
+	_, _ = js.Publish(ctx, "n.leaf.456.value", []byte("22"))
+	_, _ = js.Publish(ctx, "n.leaf.456.value", []byte("23"))
+	_, _ = js.Publish(ctx, "n.leaf.456.value", []byte("24"))
+	_, _ = js.Publish(ctx, "n.leaf.456.value", []byte("25"))
+	_, _ = js.Publish(ctx, "n.leaf.456.value", []byte("26"))
+
+	si, err := stream.Info(ctx, jetstream.WithSubjectFilter("n.456.>"))
+	if err != nil {
+		return fmt.Errorf("Error getting stream info: %w", err)
+	}
+
+	log.Println("Number of leaf stream messages: ", si.State.Msgs)
+
+	return nil
+}
+
+func hubSourceLeafStream(srv *server.Server) error {
+	url := srv.ClientURL()
+
+	nc, err := nats.Connect(url)
+	if err != nil {
+		return fmt.Errorf("Error connecting: %w", err)
+	}
+
+	defer func() {
+		_ = nc.Drain()
+	}()
+
+	js, err := jetstream.NewWithDomain(nc, "hub")
+	if err != nil {
+		return fmt.Errorf("Error creating Jetstream: %w", err)
+	}
+
+	cfg := jetstream.StreamConfig{
+		Name:     "NODES-LEAF",
+		Subjects: []string{"n.leaf.>"},
+		Sources: []*jetstream.StreamSource{
+			{
+				Name:   "NODES-LEAF",
+				Domain: "leaf",
+			},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stream, err := js.CreateStream(ctx, cfg)
+	if err != nil {
+		return fmt.Errorf("Error creating stream on hub: %w", err)
+	}
+
+	checkFor(5*time.Second, 100*time.Millisecond, func() error {
+		si, err := stream.Info(ctx, jetstream.WithSubjectFilter("n.leaf.456.>"))
+		if err != nil {
+			return fmt.Errorf("Error getting stream info: %w", err)
+		}
+
+		if si.State.Msgs != 5 {
+			return fmt.Errorf("Returned wrong number of messages: %v", si.State.Msgs)
+		}
+
+		log.Println("Number of leaf->hub sourced stream messages: ", si.State.Msgs)
 		return nil
 	})
 
